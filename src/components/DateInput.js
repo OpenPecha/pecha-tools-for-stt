09a391c12b6f215fe9489b73aa274689d579d4d4
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-09-12 13:07:52 +0530

    selec date and time

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const DateInput = ({ label, selectedDate, handleDateChange }) => {
   return (
-    <div className="form-control w-80 sm:w-fit">
+    <div className="form-control w-fit">
       <label className="label" htmlFor={label}>
         <span className="label-text">{label}:</span>
       </label>
       <input
         name={label}
-        type="date"
+        type="datetime-local"
         className="input input-bordered max-w-xs"
         value={selectedDate}
         onChange={handleDateChange}
+        max={new Date().toISOString().slice(0, 16)}
+        min={"2021-01-01T00:00"}
       />
     </div>
   );
