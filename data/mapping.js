10ec7cffcbe869503e995e2a6eac22ddfd714ff9
Sonam Phuntsog <spsither@gmail.com>
2023-09-15 12:37:48 +0530

    test: check mapping from psql

--- data/mapping.js
+++ data/mapping.js
@@ -1,6 +1,5 @@
 const mappingObj =
 {
-    "spsither@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
     "tashitsering@esukhia.org": { url: "work.pecha.tools/glyph_annotation_ga/?session=tashitsering@esukhia.org", department: "ocr" },
     "tenkus47@gmail.com" : { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
 
