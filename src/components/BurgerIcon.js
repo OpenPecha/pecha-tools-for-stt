bca5bd68786cee6fd17e91e078c85d53085e3333
Sonam Phuntsog <spsither@gmail.com>
2023-09-19 22:07:30 +0530

    ui: change burger icon color

--- src/components/BurgerIcon.js
+++ src/components/BurgerIcon.js
@@ -3,7 +3,7 @@ export default function BurgerIcon({ setShowSidebar}) {
       <svg
         onClick={() => setShowSidebar((prev) => !prev)}
         className="fixed z-30 flex items-center cursor-pointer top-2 right-10"
-        fill="#384451"
+        fill="#46b574"
         viewBox="0 0 100 80"
         width="40"
         height="40"
