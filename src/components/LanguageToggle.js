33915519bf390847b2554f3c0041e73c6c3aae06
Sonam Phuntsog <spsither@gmail.com>
2023-09-25 18:12:35 +0530

    fix: use className

--- src/components/LanguageToggle.js
+++ src/components/LanguageToggle.js
@@ -8,10 +8,10 @@ const Navbar = () => {
     let { languageSelected, setLanguageSelected } = value;
     return (
         <>
-            <label class="relative inline-flex items-center mb-5 cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" onChange={() => setLanguageSelected((prev) => prev === "en" ? "bo" : "en")} />
-                <div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{languageSelected}</span>
+            <label className="relative inline-flex items-center mb-5 cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" onChange={() => setLanguageSelected((prev) => prev === "en" ? "bo" : "en")} />
+                <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{languageSelected}</span>
             </label>
         </>
     );
