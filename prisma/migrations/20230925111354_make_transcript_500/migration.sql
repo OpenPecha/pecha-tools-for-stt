debf86c0b016091dcc0b63348819b40a66ca7837
Sonam Phuntsog <spsither@gmail.com>
2023-09-25 17:10:29 +0530

    fix: make transcription bigger

---
+++ prisma/migrations/20230925111354_make_transcript_500/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Task" ALTER COLUMN "inference_transcript" SET DATA TYPE VARCHAR(500),
+ALTER COLUMN "transcript" SET DATA TYPE VARCHAR(500),
+ALTER COLUMN "reviewed_transcript" SET DATA TYPE VARCHAR(500),
+ALTER COLUMN "final_transcript" SET DATA TYPE VARCHAR(500);
