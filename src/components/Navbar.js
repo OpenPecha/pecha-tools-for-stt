4ec66586fb2f9d6ed6ad3c6413217d2250e8fe78
Sonam Phuntsog <spsither@gmail.com>
2023-09-26 08:49:48 +0530

    fix: navbar user className not class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ import Sidebar from "./Sidebar";
 const Navbar = () => {
   return (
     <>
-      <nav class="border-gray-200 bg-gray-50">
-        <div class="lg:hidden flex flex-wrap items-center space-x-2 mx-auto p-4">
+      <nav className="border-gray-200 bg-gray-50">
+        <div className="lg:hidden flex flex-wrap items-center space-x-2 mx-auto p-4">
           <button type="button">
             <AiOutlineMenu className="w-6 h-6" />
           </button>
-          <p class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <p className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             STT
           </p>
         </div>
