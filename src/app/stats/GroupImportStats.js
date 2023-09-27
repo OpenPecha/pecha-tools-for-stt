836856b51ad43ac4b87e4fb296092d5e2c14682c
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-09-27 13:38:22 +0530

    fix: bgcolor for diff dept

--- src/app/stats/GroupImportStats.js
+++ src/app/stats/GroupImportStats.js
@@ -6,32 +6,30 @@ const GroupImportStats = ({ groupStat }) => {
 
   // Function to generate a random color based on a seed (unique ID)
   const generateRandomColor = (seed) => {
-    // List of available background colors in Tailwind CSS
+    // List of available background colors in hex format
     const colors = [
-      "red",
-      "blue",
-      "green",
-      "yellow",
-      "orange",
-      "amber",
-      "lime",
-      "emerald",
-      "teal",
-      "cyan",
-      "sky",
-      "indigo",
-      "violet",
-      "purple",
-      "fuchsia",
-      "pink",
+      "#EF4444",
+      "#3B82F6",
+      "#10B981",
+      "#F97316",
+      "#FBBF24",
+      "#F59E0B",
+      "#84CC16",
+      "#10B981",
+      "#06B6D4",
+      "#0EA5E9",
+      "#6366F1",
+      "#D946EF",
+      "#EC4899",
+      "#14B8A6",
+      "#F472B6",
+      "#9C4AED",
       // Add more colors as needed
     ];
-
     // Use the seed to select a random color from the list
     const randomColor = colors[Math.floor(Math.abs(seed) % colors.length)];
-
     // Return the Tailwind CSS class for the selected color
-    return `bg-${randomColor}-500`;
+    return `${randomColor}`;
   };
 
   return (
@@ -40,14 +38,15 @@ const GroupImportStats = ({ groupStat }) => {
         <div
           key={group.id}
           className={`
-          ${generateRandomColor(
-            group.department_id
-          )} shadow-md rounded-md p-4 ${
-            group.taskImportCount < importedThreshold
-              ? "border-4 border-red-500"
-              : ""
-          } 
+           shadow-md rounded-md p-4 ${
+             group.taskImportCount < importedThreshold
+               ? "border-4 border-red-500"
+               : ""
+           } 
          `}
+          style={{
+            backgroundColor: `${generateRandomColor(group.department_id)}`,
+          }}
         >
           <div className="flex justify-center items-center gap-5">
             <div className="text-2xl font-bold">{group.name}</div>
