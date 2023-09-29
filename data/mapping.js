070ebdde379f0bd6e4b14754af9f61042e889e55
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-09-29 14:11:39 +0530

    Update mapping.js

--- data/mapping.js
+++ data/mapping.js
@@ -1,8 +1,8 @@
 const mappingObj =
 {    
     "mm986@cam.ac.uk": { url: "https://convention-iframe.onrender.com/?base=Corr1-review&session=mm986@cam.ac.uk", department: "ocr" },
-    "mariekemeelen@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-20230809&session=mariekemeelen@gmail.com", department: "ocr" },
-    "tashitsering@esukhia.org": { url: "https://convention-iframe.onrender.com/?base=Corr1-review&session=tashitsering@esukhia.org", department: "ocr" },
+    "mariekemeelen@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-review&session=mariekemeelen@gmail.com", department: "ocr" },
+    "tashitsering@esukhia.org": { url: "https://convention-iframe.onrender.com/?base=Manual1-review&session=tashitsering@esukhia.org", department: "ocr" },
     "tenkus47@gmail.com" : { url: "https://work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
     "shugtso@monlam.ai" : { url: "https://work.pecha.tools/glyph_annotation_ga/?session=shugtso@monlam.ai", department: "ocr" },
     
