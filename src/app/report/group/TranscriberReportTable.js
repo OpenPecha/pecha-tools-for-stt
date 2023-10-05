6aeead3e22b44ebb9d586a675b2772f52b2e2e2d
TENZIN TAMDIN <tenzintamdin@TENZINs-MacBook-Air.local>
2023-10-05 13:52:10 +0530

    feat:refactor and add color based on percentage

--- src/app/report/group/TranscriberReportTable.js
+++ src/app/report/group/TranscriberReportTable.js
@@ -1,23 +1,68 @@
 import Link from "next/link";
 import React from "react";
 import { calculatePay } from "@/lib/calculatePay";
+import { calculatePercent } from "@/lib/calculatePercent";
 const TranscriberReportTable = ({ usersStatistic, selectGroup }) => {
+  
+  const glideGreentoRed = (num1, num2) => {
+    // Calculate the percentage
+    const percentage = calculatePercent(num1, num2);
+    // if else to return the color based on the percentage
+    if (percentage > 80) {
+      return "bg-[#ff0000]";
+    } else if (percentage > 60) {
+      return "bg-[#ffa700]";
+    } else if (percentage > 40) {
+      return "bg-[#fff400]";
+    } else if (percentage > 20) {
+      return "bg-[#a3ff00]";
+    } else {
+      return "bg-[#2cba00]";
+    }
+  };
+
+  const glideRedtoGreen = (num1, num2) => {
+    // Calculate the percentage
+    const percentage = calculatePercent(num1, num2);
+    // if else to return the color based on the percentage
+    if (percentage > 80) {
+      return "bg-[#2cba00]";
+    } else if (percentage > 60) {
+      return "bg-[#a3ff00]";
+    } else if (percentage > 40) {
+      return "bg-[#fff400]";
+    } else if (percentage > 20) {
+      return "bg-[#ffa700]";
+    } else {
+      return "bg-[#ff0000]";
+    }
+  };
+
   return (
     <>
       <div className="overflow-x-auto shadow-md sm:rounded-lg w-11/12 md:w-4/5 max-h-[80vh]">
         <table className="table  ">
           {/* head */}
-          <thead className="text-gray-700 bg-gray-50">
+          <thead
+            className="text-gray-700 bg-gray-50
+          "
+          >
             <tr>
               <th>Transcriber Name</th>
-              <th>Task Submitted</th>
-              <th>Task Reviewed</th>
+              <th>
+                Task <br /> Submitted
+              </th>
+              <th>
+                Task <br /> Reviewed
+              </th>
               <th>Reviewed %</th>
               <th>Submitted Min.</th>
               <th>Reviewed Min.</th>
               <th>Reviewed min %</th>
               <th>Task Corrected %</th>
-              <th>Reviewed Syllable count</th>
+              <th>
+                Reviewed <br /> Syllable count
+              </th>
               <th>Rs.</th>
             </tr>
           </thead>
@@ -29,28 +74,29 @@ const TranscriberReportTable = ({ usersStatistic, selectGroup }) => {
                 </td>
                 <td>{user.noSubmitted}</td>
                 <td>{user.noReviewed}</td>
-                <td>
-                  {user.noReviewed !== 0
-                    ? ((user.noReviewed / user.noSubmitted) * 100).toFixed(2)
-                    : 0}
+                <td
+                  className={`${glideRedtoGreen(
+                    user.noReviewed,
+                    user.noSubmitted
+                  )}
+                  `}
+                >
+                  {calculatePercent(user.noReviewed, user.noSubmitted)}
                 </td>
                 <td>{user.submittedInMin}</td>
                 <td>{user.reviewedInMin}</td>
                 <td>
-                  {user.reviewedInMin !== 0
-                    ? (
-                        (user.reviewedInMin / user.submittedInMin) *
-                        100
-                      ).toFixed(2)
-                    : 0}
+                  {calculatePercent(user.reviewedInMin, user.submittedInMin)}
                 </td>
-                <td>
-                  {user.noReviewedCorrected !== 0
-                    ? (
-                        (user.noReviewedCorrected / user.noReviewed) *
-                        100
-                      ).toFixed(2)
-                    : 0}
+                <td
+                  className={`${glideGreentoRed(
+                    user.noReviewedCorrected,
+                    user.noReviewed
+                  )} 
+
+                  `}
+                >
+                  {calculatePercent(user.noReviewedCorrected, user.noReviewed)}
                 </td>
                 <td>{user.syllableCount}</td>
                 <td>
---
+++ src/lib/calculatePercent.js
@@ -0,0 +1,4 @@
+export const calculatePercent = (num1, num2) => {
+  if (num1 === 0) return 0;
+  return ((num1 / num2) * 100).toFixed(2);
+};
