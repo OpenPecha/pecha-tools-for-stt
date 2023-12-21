64e1e283e29427ac6a77de01dfdbfb6abe405299
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-12-21 19:44:53 +0530

    Update mapping.js

--- data/mapping.js
+++ data/mapping.js
@@ -20,19 +20,6 @@ const mappingObj =
     "breezynyibum@gmail.com": { url: "https://work.pecha.tools/glyph_ann_review_ga/?session=breezynyibum@gmail.com", department: "ocr" },
     "jamperthupten@gmail.com": { url: "https://work.pecha.tools/glyph_ann_review_gb/?session=jamperthupten@gmail.com", department: "ocr" },
 
-    "kungachoden1983@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=kungachoden1983@gmail.com", department: "ocr" },
-    "tsedhak123@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=tsedhak123@gmail.com", department: "ocr" },
-    "dhephala@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=dhephala@gmail.com", department: "ocr" },
-    "tthegchok1985@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=tthegchok1985@gmail.com", department: "ocr" },
-    "tenzintherchen154@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=tenzintherchen154@gmail.com", department: "ocr" },
-
-    "khedup62@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=khedup62@gmail.com", department: "ocr" },
-    "ngawangsamten1989@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=ngawangsamten1989@gmail.com", department: "ocr" },
-    "dhondup995@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=dhondup995@gmail.com", department: "ocr" },
-    "thubsamtharchin1982@gmail.com": { url: "https://convention-iframe.onrender.com/?base=line_to_text_ga&session=thubsamtharchin1982@gmail.com", department: "ocr" },
-
-    "Kungalhundup1959@gamil.com": { url: "https://work.pecha.tools/ltt_review_ga/?session=Kungalhundup1959@gamil.com", department: "ocr" },
-
     "tsultrimloden1@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-review&session=tsultrimloden1@gmail.com", department: "ocr" },
     "sherabchokgyal321@gmail.com.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-review&session=sherabchokgyal321@gmail.com", department: "ocr" },
 
