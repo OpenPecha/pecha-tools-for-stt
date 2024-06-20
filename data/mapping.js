5d4dc00986f779abaf9ff247eeb67627eb155e7e
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2024-06-20 11:29:42 +0530

    Update mapping.js

--- data/mapping.js
+++ data/mapping.js
@@ -5,7 +5,7 @@ const mappingObj =
     "tenkus47@gmail.com": { url: "https://stt.pecha.tools/?session=lhatsoyeshi@gmail.com", department: "stt" },
     
 
-    "sangmotenz79@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_ga/?session=sangmotenz79@gmail.com", department: "ocr" },
+    "tashitsering@esukhia.org": { url: "https://work.pecha.tools/glyph_annotation_ga/?session=tatse", department: "ocr" },
     "dakung132@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_ga/?session=dakung132@gmail.com", department: "ocr" },
     "wpema76@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_ga/?session=wpema76@gmail.com", department: "ocr" },
     "tenzinkals11@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_ga/?session=tenzinkals11@gmail.com", department: "ocr" },
@@ -14,27 +14,9 @@ const mappingObj =
     "sonamgyatso3767@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_gb/?session=sonamgyatso3767@gmail.com", department: "ocr" },
     "yeshidolma5882@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_gb/?session=yeshidolma5882@gmail.com", department: "ocr" },
     "tenphuns@gmail.com": { url: "https://work.pecha.tools/glyph_annotation_gb/?session=tenphuns@gmail.com", department: "ocr" },
-    
-    
 
     "breezynyibum@gmail.com": { url: "https://work.pecha.tools/glyph_ann_review_ga/?session=breezynyibum@gmail.com", department: "ocr" },
-    "jamperthupten@gmail.com": { url: "https://work.pecha.tools/glyph_ann_review_gb/?session=jamperthupten@gmail.com", department: "ocr" },
-
-    "tsultrimloden1@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-review&session=tsultrimloden1@gmail.com", department: "ocr" },
-    "sherabchokgyal321@gmail.com.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-review&session=sherabchokgyal321@gmail.com", department: "ocr" },
-
-
-    "tsukpo777@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-20230809&session=tsukpo777@gmail.com", department: "ocr" },
-    "tsultrimgyaltsen22@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-20230809&session=tsultrimgyaltsen22@gmail.com", department: "ocr" },
-    "kalsanggyaltsen@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-20230809&session=kalsanggyaltsen@gmail.com", department: "ocr" },
-    "tsewangdrukgyal@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Manual1-20230809&session=tsewangdrukgyal@gmail.com", department: "ocr" },
-
-
-    "sherabphuntsok64@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-20230809&session=sherabphuntsok64@gmail.com", department: "ocr" },
-    "tsukphudrabsal@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-20230809&session=tsukphudrabsal@gmail.com", department: "ocr" },
-    "Gyamstsholama2023@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-20230809&session=Gyamstsholama2023@gmail.com", department: "ocr" },
-    "sherapwoser124@gmail.com": { url: "https://convention-iframe.onrender.com/?base=Corr1-20230809&session=sherapwoser124@gmail.com", department: "ocr" },
-
+    "jamperthupten@gmail.com": { url: "https://work.pecha.tools/glyph_ann_review_gb/?session=jamperthupten@gmail.com", department: "ocr" }
     
 }
 
