053ebf6ff066235737866a336c99a7092d894856
tentamdin <tgtamdin3@gmail.com>
2024-06-28 17:27:32 +0530

    revised calculate pay

--- src/lib/calculatePay.js
+++ src/lib/calculatePay.js
@@ -1,10 +1,22 @@
-export const calculatePay = (groupID, reviewedInMin, syllableCount, reviewedCount) => {
-    const stt_ab_groups = [1, 2, 7];
-    groupID = Number(groupID)
-    if (stt_ab_groups.includes(groupID)) {
-        return (reviewedInMin * 5 + reviewedCount * 2).toFixed(2);
-    }
-    else {
-        return (reviewedInMin * 5 + syllableCount * 0.4).toFixed(2);
-    }
+export const calculatePay = (
+  groupID,
+  reviewedInMin,
+  trashedInMin,
+  syllableCount,
+  reviewedCount
+) => {
+  const stt_ab_groups = [1, 2, 7];
+  const stt_cs_groups = [3, 4, 6];
+  groupID = Number(groupID);
+  if (stt_ab_groups.includes(groupID)) {
+    return (reviewedInMin * 5 + reviewedCount * 2).toFixed(2);
+  } else if (stt_cs_groups.includes(groupID)) {
+    return ((reviewedInMin + trashedInMin) * 5 + syllableCount * 0.33).toFixed(
+      2
+    );
+  } else {
+    return ((reviewedInMin + trashedInMin) * 5 + syllableCount * 0.3).toFixed(
+      2
+    );
+  }
 };
