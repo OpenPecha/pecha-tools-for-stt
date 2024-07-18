cd505c0ad59479d4d910f86ff0761383cae9f519
tenkal <tenkalden007@gmail.com>
2024-07-18 16:20:31 +0530

    updated mapping

--- data/mapping.js
+++ data/mapping.js
@@ -61,6 +61,14 @@ const mappingObj = {
     url: "https://work.pecha.tools/glyph_ann_review_gb/?session=breezynyibhum@gmail.com",
     department: "ocr",
   },
+  "nordhentsering57@gmail.com": {
+    url: "https://work.pecha.tools/glyph-cropping-ga/?session=nordhentsering57@gmail.com",
+    department: "ocr",
+  },
+  "dhekharwork123@gmail.com": {
+    url: "https://work.pecha.tools/glyph-cropping-ga/?session=dhekharwork123@gmail.com",
+    department: "ocr",
+  },
 };
 
 export const mapping = new Map(Object.entries(mappingObj));
