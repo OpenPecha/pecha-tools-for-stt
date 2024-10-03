f412b2018b428ab9eb70984d0a1abdf13a42900c
tentamdin <tgtamdin3@gmail.com>
2024-10-03 17:44:46 +0530

    revert calculate %

--- src/lib/calculatePercent.js
+++ src/lib/calculatePercent.js
@@ -1,6 +1,4 @@
 export const calculatePercent = (num1, num2) => {
   if (num1 === 0 || num2 === 0) return 0;
-  // if it is greater than 100 rerturn 100
-  if (num1 > num2) return 100.0;
   return ((num1 / num2) * 100).toFixed(2);
 };
