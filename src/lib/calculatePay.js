051db3d72fcd1841458bda64c600a643c4694c9a
tentamdin <tgtamdin3@gmail.com>
2024-11-25 11:26:04 +0530

    add new ab group

--- src/lib/calculatePay.js
+++ src/lib/calculatePay.js
@@ -5,7 +5,7 @@ export const calculatePay = (
   syllableCount,
   reviewedCount
 ) => {
-  const stt_ab_groups = [1, 2, 7];
+  const stt_ab_groups = [1, 2, 7, 24, 26];
   const stt_cs_groups = [3, 4, 6];
   groupID = Number(groupID);
   if (stt_ab_groups.includes(groupID)) {
